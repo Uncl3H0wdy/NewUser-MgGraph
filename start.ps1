d34d43966566f029d0cc9f4ea92ac090bb4aa495
Keil Carpenter <keil.carpenter@global.ntt>
2025-07-30 15:32:23 +1200

    refactor

--- start.ps1
+++ start.ps1
@@ -63,7 +63,7 @@ $dlNames = @('DL All Users')
 # Loop until the user selects a valid number
 while($true){
     try {
-         $userInput = [int](Read-Host "Please choose from one of the following:`n1: The user reports to the CEO.`n2: The user has direct reports.`n3: None of the above.")
+         $userInput = [int](Read-Host "Please choose from one of the following:`n[1]: The user reports to the CEO.`n[2]: The user has direct reports.`n[3]: None of the above.")
          # Checks if the input matches exactly '1', '2' or '3'
          if($userInput -match '\b[1-3]\b'){
              # Check the value of $doneSafe and add it to the $groups Array
@@ -80,11 +80,11 @@ while($true){
  while($true){
     try {
          # Validates the users input is an integer
-         $userInput = [int](Read-Host "Select the location of the user:`n1: Wellington.`n2: Auckland.`n3: Christchurch.")
+         $userInput = [int](Read-Host "Select the location of the user:`n[1]: Wellington.`n[2]: Auckland.`n[3]: Christchurch.")
          
          # Checks if the input matches exactly '1', '2' or '3'
          if($userInput -match '\b[1-3]\b'){
-             # Check the value of $doneSafe and add it to the $groups Array
+             # Check the value of $userInput and add the corresponding value to the $dlNames array
              if ($userInput -eq 1) {$dlNames += "DL WEL Users"}
              elseif($userInput -eq 2){$dlNames += "DL Te Whare Rama"}
              elseif($userInput -eq 3){$dlNames += 'DL CHC Users'}
@@ -143,10 +143,10 @@ $safeSenders = @(
 
 $target = Get-Mailbox $user.UserPrincipalName
 
-foreach($sender in $safeSenders){
+foreach($thissender in $safeSenders){
     try{
-        Set-MailboxJunkEmailConfiguration $target.Name -TrustedSendersAndDomains @{Add = $sender}
-        Write-Host "Successfully added $sender to the Trusted Senders and Domains list" -ForegroundColor Green
+        Set-MailboxJunkEmailConfiguration $target.Name -TrustedSendersAndDomains @{Add = $thissender}
+        Write-Host "Successfully added $thissender to the Trusted Senders and Domains list" -ForegroundColor Green
     }catch{
         Write-Host $_
     }
